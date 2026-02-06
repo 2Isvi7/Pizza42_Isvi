55e7d0d95d4d8c64f74978667e9856532374bb12
Isvi Acuna <z2isvi7@hotmail.com>
2026-02-06 12:33:07 -0600

    Fix case sensitivity in css import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import LandingPage from "./components/LandingPage";
 import Dashboard from "./components/Dashboard";
-import "./App.css";
+import "./app.css";
 
 function App() {
   const { isLoading, isAuthenticated, error } = useAuth0();
